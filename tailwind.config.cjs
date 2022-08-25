44381f198d821739188397964a1910ed50d56600
Puk Sørensenn <pukx0525@stud.kea.dk>
2022-08-25 15:48:12 +0200

    Farver og Fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      H1red: "#F23827",
+      H2blue: "#221F73",
+      H3dblue: "#161354",
+      Pblack: "#000000",
+      Pgray: "#494949",
+      BackgroundOW: "#F3EEEE",
+      Pwhite: "#FFFFFF",
+      Decogreen: "#D4E581",
+    },
+    extend: {
+      fontFamily: "'Biryani', system-ui, sans-serif",
+      fontFamily: "'Noto Sans JP', system-ui, sans-serif",
+      fontFamily: "'Rubik Dirt', system-ui, cursive",
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
